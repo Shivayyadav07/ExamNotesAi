fc1446265eee4c030c71f5e1ba20eca593740926
Shivayyadav07 <shivaayyadav07@gmail.com>
2026-09-20 17:06:20 +0530

    Update CORS origin for production environment

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@
  const app = express()
  
  app.use(cors({
-     origin:"http://localhost:5173"
+     origin:"https://examnotesaiclient-39p8.onrender.com"
      ,credentials:true
      ,methods:["GET","POST","PUT","DELETE","OPTIONS"]
  }))
